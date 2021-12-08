f75b01c1b69d9c8b6d71388af7b777e84e47cc79
eseunghwan <reignstarbits@gmail.com>
2021-12-08 11:51:41 +0900

    version 2021.12.08

--- brue/__init__.py
+++ brue/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from ._core import brue, brueStore, brueRoute, brueElement
 
-__version__ = "2021.12.07"
+__version__ = "2021.12.08"
 __author__ = "Lee Seung Hwan"
 __mail__ = "shlee0920@naver.com"
